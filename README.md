08313579594abc5dd4bf0cb0212e299f86bfd84b
YYago <877675862@qq.com>
2018-03-16 03:31:10 +0800

    add fs_wf() And update to V1.0.4.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # node-modules-custom
 
-Node.js 内置模块自定义改装。
+Node.js 内置模块：`fs.writeFile`、`fs.writeFileSync`、`fs.mkdirSync`自定义改装。
 
-**下载使用须知:**  这个npm package 似乎没有什么普遍适用性，开发者自用(穷，买不起私有服务)，如果使用到你自己的项目中，请自己审查代码是否适合，避免造成的损失(因为目的在避免触发错误callback导致运行中断，所以可能会对BUG检查、性能等方面带来影响)！！！
+> 经常会进行批量写入、追加操作，但当目录不存在时（且目录层级多时）往往会报错并终止运行，我希望可以尽量避免或者减少出现在路径问题上发生的错误回调，仅此而已。
+
+**下载使用须知:**  这个npm package 似乎没有什么普遍适用性，开发者自用(穷，买不起私有服务)，如果使用到你自己的项目中，请自己审查代码是否适合。
 
 ## APIs
 
@@ -14,17 +16,24 @@ Node.js 内置模块自定义改装。
  *  `data`:  内容.
  *  `isReplaceAndCover`: 类型：Boolean;  对于已经存在的文件如何处理，`true`：将会进行处理（怎么处理请参考 `fs.writeFileSync()`的`options:{}`进行）。`false`：则忽略，什么都不做。
  * `options`：arguments[3]  默认值 `{encoding:'utf-8',flag:'w'}` ，当设置为`{flag:'a'}` 的时候将对已经存在的文件执行追加操作。
-    >{mode:'0o666'} 可能带来一些权限问题，这里就不指定了。
-
-> examp：
->
-> ```xxx.fs_wfSync('a.txt','hello',true,{flag:"a"});```
+    > examp：
+    >
+    >`xxx.fs_wfSync('a.txt','hello',true,{flag:"a"});`
+    >
+    >`{mode:'0o666'}` 可能带来一些权限问题(不确定)，这里就不指定了。
 
 ### 2、 .fs_mkdirSync ( pathString )
 
 `fs.mkdirSync()` 的改进，尽可能避免触发 ‘文件夹已经存在’ 的callback。
 
 * `pathString`: 文件夹路径。
+
+### 3、 .fs_wf(filePath, data, isReplaceAndCover)
+
+ *  `filePath`:  文件路径.
+ *  `data`:  内容.
+ *  `isReplaceAndCover`: 类型：Boolean;  对于已经存在的文件如何处理，`true`：将会进行处理（怎么处理请参考 `fs.writeFile()`的`options:{}`进行）。`false`：则忽略，什么都不做。
+ * `options`：arguments[3]  默认值 `{encoding:'utf-8',flag:'w'}` ，当设置为`{flag:'a'}` 的时候将对已经存在的文件执行追加操作。
 
 ## 使用方法
 
@@ -34,26 +43,34 @@ Install:
  npm install --save node-modules-custom
  ```
 
-Require:
+Example:
  ```JavaScript
  const myWriteFile = require('node-modules-custom');
 
  //.....
- /*
-   If a.txt didn't exist will create it,else add the content:'hello' to it.
- */
- myWriteFile.fs_wfSync('a.txt','hello',true,{flag:"a"});
+
+// If a.txt didn't exist will create it,else add the content:'hello' to it.
+
+myWriteFile.fs_wfSync('a.txt','hello',true,{flag:"a"});
  
 
- /*
-   If a.txt didn't exist will create it,else rewrite it.
- */
+// If a.txt didn't exist will create it,else rewrite it.
+
 myWriteFile.fs_wfSync('a.txt','hello',true);
 
 
-/*
-   If a.txt didn't exist will create it,else notring.
-*/
+// If a.txt didn't exist will create it,else notring.
+
 myWriteFile.fs_wfSync('a.txt','hello',false);
 
+// Create multi-level directory(as long path)
+// Note: Don't add a file name at the end, otherwise the file name will be created as a directory unless you want to do it.
+myWriteFile.fs_mkdirSync('a/b/c/d/e/f/g/h/i');
+
+
+// asynchronous
+
+// If a.txt didn't exist will create it,else notring.
+
+myWriteFile.fs_wf('a.txt','hello',false);
  ```
--- index.js
+++ index.js
@@ -23,7 +23,6 @@ function cs_fs_wfSync(filePathString, datas) {
             }
         }
         fs.writeFileSync(filePathString, datas, argms);
-        console.log(filePathString + '写入成功! success!')
     } else {
         var fooFPS = filePathString;
         let fpDirname = path.dirname(fooFPS);
@@ -37,11 +36,12 @@ function cs_fs_wfSync(filePathString, datas) {
             }
         }
         fs.writeFileSync(filePathString, datas, argms);
-        console.log(filePathString + '写入成功! success!')
     }
 
 }
 function cs_fs_wf(filePathString, datas) {
+    let argms = arguments[2] || { encoding: 'utf8', flag: 'w' };
+
     if (os == "Windows_NT") {
         let spRegexp = new RegExp(/\\\\/g);
         if (spRegexp.test(filePathString) == false) {
@@ -59,9 +59,10 @@ function cs_fs_wf(filePathString, datas) {
                 fs.mkdirSync(prfixDir);
             }
         }
-        fs.writeFile(filePathString, datas, argms, (err) => {
-            if (err) throw err;
-            console.log(filePathString + '写入成功! success!');
+        fs.writeFile(filePathString, datas, argms,(err)=>{
+            if(err){
+                console.log(`${err}`)
+            }
         });
     } else {
         var fooFPS = filePathString;
@@ -75,11 +76,13 @@ function cs_fs_wf(filePathString, datas) {
                 fs.mkdirSync(prfixDir);
             }
         }
-        fs.writeFile(filePathString, datas, argms, (err) => {
-            if (err) throw err;
-            console.log(filePathString + '写入成功! success!');
+        fs.writeFile(filePathString, datas, argms,(err)=>{
+            if(err){
+                console.log(`${err}`)
+            }
         });
     }
+
 }
 /**
  * 自定义的 fs.writeFileSync(),试图解决长路径文件不能创建的问题。
@@ -105,15 +108,52 @@ function fs_wfSync(filePath, data, isReplaceAndCover) {
     // 判断 filePath 是否已经存在。
     if (fs.existsSync(filePath) == false) {
         cs_fs_wfSync(filePath, data, options);
-        console.log(`${filePath} 创建成功！`)
+        console.log(`${filePath}    创建成功！`)
     } else if (fs.existsSync(filePath) && isReplaceAndCover == true) {
         fs.writeFileSync(filePath, data, options);
-        console.log(`${filePath} 创建成功！`)
+        console.log(`${filePath}    创建成功！`)
     } else if (fs.existsSync(filePath) && isReplaceAndCover == false) {
         // 什么都不做
-        console.log(filePath + '已经存在并忽略.It has already existed and skipped.');
+        console.log(filePath + '    已经存在并忽略.It has already existed and skipped.');
     }
 }
+
+/**
+ * 自定义的 fs.writeFile(),试图解决长路径文件不能创建的问题。
+ * 
+ * 
+ * @param {String} filePath 文件路径
+ * @param {String} data 内容(当创建文件夹时，可以使用任意值，但必须声明)
+ * @param {Boolean} isReplaceAndCover  对于已经存在的文件或文件夹如何处理，true：将会进行覆盖并替换。false：则忽略，什么都不做。
+ * @argument {Object} object 默认 {"encoding": "utf8", "flag": "w" }
+ * 
+ * 
+ * 当文件路径的父路径文件夹不存在时创建文件常常会导致写入失败报错(会中断运行)，我们试着先创建文件的父路径目录，最后再创建文件（写入文件）.
+ * 
+ * 顺便加入是否对已有文件的操作功能：是忽略已存在文件还是直接覆盖。有时候还是有用的，比如批量创建文件的时候。
+ * 
+ * NOTE: 
+ * 
+ * 1. 对已有文件进行覆盖、追加，请自行通过添加{encoding:"",flag:"",mode:""}处理。
+ * 2. 如果不是长路径还是不要使用这个函数，没有考虑：./x.x 和 x.x 的情况(太懒，不想测试)，不知道能不能创建成功。 
+ */
+function fs_wf(filePath, data, isReplaceAndCover) {
+    let options = arguments[3] || {encoding:'utf-8',flag:'w'}; // 用于fs.writeFile()的options
+    // 判断 filePath 是否已经存在。
+    if (fs.existsSync(filePath) == false) {
+        cs_fs_wf(filePath, data, options);
+    } else if (fs.existsSync(filePath) && isReplaceAndCover == true) {
+        fs.writeFile(filePath, data, options,(err)=>{
+            if(err){
+                console.log(`${err}`)
+            }
+        });
+    } else if (fs.existsSync(filePath) && isReplaceAndCover == false) {
+        // 什么都不做
+        console.log(filePath + '    已经存在并忽略.It has already existed and skipped.');
+    }
+}
+
 /**
  * 
  * 自定义的 fs.mkdirSync()，已经存在的文件夹会跳过,不会触发其callback。
@@ -150,14 +190,17 @@ function fs_mkdirSync(pathString) {
                 }
             }
             fs.mkdirSync(pathString);
-            console.log(pathString+'ok');
+            console.log(pathString+'    ok');
         }
     }else{
-        console.log(pathString+"文件夹已经存在，什么都没做。The folders already exist and nothing to do.")
+        console.log(pathString+"    文件夹已经存在，什么都没做。The folders already exist and nothing to do.")
     }
 
 }
+
 module.exports = {
     fs_wfSync,
-    fs_mkdirSync
+    fs_wf,
+    fs_mkdirSync,
 }
+
